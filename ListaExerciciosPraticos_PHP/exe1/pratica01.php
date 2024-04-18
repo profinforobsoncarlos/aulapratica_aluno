1090315192088138bbc973e75cf5c9bb53182a35
profinforobsoncarlos <167130842+profinforobsoncarlos@users.noreply.github.com>
2024-04-18 10:19:58 -0400

    atualização1804.1

---
+++ ListaExerciciosPraticos_PHP/exe1/pratica01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex 01</title>
+</head>
+<body>
+<!--
+Crie um programa que leia o nome e o salário de um funcionário,
+mostrando no final uma mensagem. Ex:
+Nome do Funcionário: Maria do Carmo
+Salário: R$1.850,45
+O funcionário Maria do Carmo tem um salário de R$1.850,45 em (data de hoje).
+-->
+<div>
+
+
+
+
+</div>
+</body>
+</html>
